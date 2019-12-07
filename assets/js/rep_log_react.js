0cf854fa0c3ed13c626be4f5d50d7b09f01f4193
petrero <petrero@gmail.com>
2019-12-07 14:46:26 +0200

    5.3.Props{Rendering a Component Multiple Times}
    Rendering a Component Multiple Times
    
    To really show off, change that value back to true, but let's see if we can render RepLogApp multiple times. Copy the JSX, paste, and set withHeart to false.
    
    But, as soon as we do this, the Webpack build fails! Find your terminal to see what it's complaining about:
    
      Syntax Error: Adjacent JSX elements must be wrapped in an enclosing tag
    
    This is less scary than it sounds. It's not that you can't put components next to each other like this, it just means that there must be just one element all the way at the top of our JSX tree. Each component also needs to follow this rule. And, RepLogApp already is: it has one top-level element: the h2.

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render }  from 'react-dom';
 import RepLogApp from './RepLog/RepLogApp';
 
-const shouldShowHeart = false;
+const shouldShowHeart = true;
 
 render(
-	<RepLogApp withHeart={shouldShowHeart}/>,
+	<RepLogApp withHeart={shouldShowHeart}/> <RepLogApp withHeart=false/>,
 	document.getElementById('lift-stuff-app')
 );
