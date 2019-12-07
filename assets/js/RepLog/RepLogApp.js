bab259652b5dc4eab5a268acdeb51a9f8a05fc6a
petrero <petrero@gmail.com>
2019-12-07 17:06:17 +0200

    8.3.Build the Static App First{Using form defaultValue}
    Using form defaultValue
    
    Let's fix the warning:
      Warning: Use the 'defaultValue' or 'value' props on <select> instead of setting 'selected' on <option>.
    first before proper styling the form. We'll talk a lot more about forms in React later. But basically, React is a control freak, it really really* wants to manage the values of any elements on your form, including which option is selected. So, instead of using selected="selected", you can use defaultValue="" on the select element and set it to the value of the option you want. I'll skip that part because the first option will be automatically selected anyways.

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -49,10 +49,10 @@ export default class RepLogApp extends Component {
 				<form className="form-inline">
 					<div className="form-group">
 						<label className="sr-only required" htmlFor="item">What did you lift?</label>
-						<select id="rep_log_item" name="item" required="required">
+						<select id="rep_log_item" /* defaultValue="cat"*/ name="item" required="required">
 							<option value="">What did you lift?</option>
 							<option value="cat">Cat</option>
-							<option value="fat_cat" selected="selected">Big Fat Cat</option>
+							<option value="fat_cat" >Big Fat Cat</option>
 							<option value="laptop">My Laptop</option>
 							<option value="coffee_cup">Coffee Cup</option>
 						</select>
