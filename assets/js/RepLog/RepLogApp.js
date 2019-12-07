b12528595877bbe877ccaca7141b1146cab809d5
petrero <petrero@gmail.com>
2019-12-07 18:09:12 +0200

    9.2.State: For Magic Updating Good Times{Adding highlightRowId State}
    Adding highlightRowId State
    
    But first, let's do some coding and add our first user interactivity! Here's the goal: when the user clicks a row in the table, I want to highlight that row.
    
    From a data standpoint, this means that, somewhere in our RepLogApp component, we need to keep track of which of these rep log rows is currently highlighted. We can do that by keeping track of the highlighted row id. Literally, we could store that row 1, 2 or 8 is highlighted.
    
    But because this data will change throughout the lifecycle of our app, we're not going to store the id in props. Nope, we're going to store it as state.
    
    Once you've decide that you have some data that needs to be stored as state, you need to initialize that value on your state. That's always done the same way: by overriding the constructor() function. The constructor of React components receive a props argument. And then, you're supposed to call super(props) to execute the parent constructor. You'll see this pattern over and over again.
    
    To set the initial state, just set the property directly: this.state equals an object, with, how about, a highlightedRowId key set to null. Nothing will be highlighted at first.

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,6 +1,14 @@
 import React, {Component} from "react";
 
 export default class RepLogApp extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			highlightedRowId: null
+		}
+	}
+
 	render() {
 		let heart = '';
 		const repLogs = [
