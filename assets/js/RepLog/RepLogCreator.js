3df0e21accd1e1665d050ae9180bd07039639da9
petrero <petrero@gmail.com>
2019-12-08 14:27:42 +0200

    19.4.New Component to Hold our Form{Introducing: Refs; Refactoring our Dumb Component to a Class}
    Introducing: Refs
    
    And now.... it's time to start bending some of the nice rules that we've been talking about. RepLogCreator is a "dumb" component. It's like a template: its main job is to render markup, not to contain state or a lot of logic. Because "dumb", presentation components only really render things, we usually create them as functions instead of a class... just because we can and we're lazy!
    
    Well... that's a good rule, but it's not always true. Right now, our handler function uses the name attribute of the input element to get a reference to the underlying DOM element. Then, it reads its value. It turns out that React has its own system for allowing you to reference the corresponding DOM element for any of our React elements. It's called "refs".
    
    Refactoring our Dumb Component to a Class
    
    There are a few ways to use this "refs" system. But, the recommended way requires your component to be a class. And, that's ok! There are some legitimate situations where a dumb component needs a class. Refs are one of those.
    
    No big deal: let's change this to a class! First, we need to also import Component from React. Then, export default class RepLogCreator extends Component. And, of course, we now need to put all of this inside a render() function. Let's indent everything one level, and close the function.
    
    Yep! Webpack is happy! Now that we have a proper class, we don't need to put handleFormSubmit() inside of render() anymore. Nope, we can access the props from anywhere as this.props.
    
    So, copy that function & the const, paste it in the class, turn it into a property, and move the const into the function. Oh, and try not to mess up the syntax like I just did.
    
    Better! Back in render(), now we'll call this.handleFormSubmit.
    
    Let's go check it out! Head back to the browser refresh! It loads... and when you submit... error! Woh! And then the page reloaded! Oh no!
    
    There are two evil problems working together against us! For the first, my bad! We need to make sure that event.preventDefault() is always the first line in a handle function. You'll see why when we refresh and try the form again.
    
    Ah yes: here is the real error:
    
      Cannot read property props of undefined
    
    coming from line 7. We know this problem: we forgot to bind our handler function to this... so this is not our RepLogCreator instance. When the page refreshed, it was because this error killed our code even before we called event.preventDefault().

--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-export default function RepLogCreator(props) {
-	const { onNewItemSubmit } = props;
+export default class RepLogCreator extends Component {
 
-	function handleFormSubmit(event) {
+	handleFormSubmit(event){
 		event.preventDefault();
 
+		const { onNewItemSubmit } = this.props;
 		console.log('Submit');
 		console.log(event.target.elements.namedItem('reps').value);
 
 		onNewItemSubmit('Big Fat Cat', event.target.elements.namedItem('reps').value);
 	}
 
-	return (
-		<form className="form-inline" onSubmit={ handleFormSubmit }>
-			<div className="form-group">
-				<label className="sr-only required" htmlFor="item">What did you lift?</label>
-				<select id="rep_log_item" /* defaultValue="cat"*/ name="item" required="required">
-					<option value="">What did you lift?</option>
-					<option value="cat">Cat</option>
-					<option value="fat_cat">Big Fat Cat</option>
-					<option value="laptop">My Laptop</option>
-					<option value="coffee_cup">Coffee Cup</option>
-				</select>
-			</div>
-			{' '}
-			<div className="form-group">
-				<label className="sr-only required" htmlFor="reps">How many times?</label>
-				<input type="number" id="rep_log_reps" name="reps" required="required" placeholder="How many times?"/>
-			</div>
-			{' '}
-			<button type="submit" className="btn btn-primary">I Lifted it!</button>
-		</form>
-	);
+	render() {
+		return (
+			<form className="form-inline" onSubmit={this.handleFormSubmit}>
+				<div className="form-group">
+					<label className="sr-only required" htmlFor="item">What did you lift?</label>
+					<select id="rep_log_item" /* defaultValue="cat"*/ name="item" required="required">
+						<option value="">What did you lift?</option>
+						<option value="cat">Cat</option>
+						<option value="fat_cat">Big Fat Cat</option>
+						<option value="laptop">My Laptop</option>
+						<option value="coffee_cup">Coffee Cup</option>
+					</select>
+				</div>
+				{' '}
+				<div className="form-group">
+					<label className="sr-only required" htmlFor="reps">How many times?</label>
+					<input type="number" id="rep_log_reps" name="reps" required="required" placeholder="How many times?"/>
+				</div>
+				{' '}
+				<button type="submit" className="btn btn-primary">I Lifted it!</button>
+			</form>
+		);
+	}
 }
 
 RepLogCreator.propTypes = {
