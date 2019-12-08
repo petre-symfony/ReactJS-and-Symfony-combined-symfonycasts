87e0db22780b8995dd2fdc1a8bb40c309f429be1
petrero <petrero@gmail.com>
2019-12-08 09:48:21 +0200

    13.3.Smart vs Dumb Components{A Smart Component Wraps a Dumb Component}
    A Smart Component Wraps a Dumb Component
    
    Yep, we're going to split RepLogApp into two components: one with all the logic and another will all the markup. Create a new file called RepLogs.js: this will be our stateless, dumb component. So, this will look a lot like RepLogList: import React from react. And then, export default function RepLogs.
    
    Next, go copy all of the code from RepLogApp's render() function and, paste it here.
    
    This new component has basically no logic, except for a tiny bit on top that's related to the markup itself.
    
    Back in RepLogApp, delete all of that code! Instead, on top, import RepLogs from ./RepLogs. And then, in render, all we need is <RepLogs />.
    
    That is it! Oh, it's great: look how pure & clean the top level component is! Our business logic is much easier to read. And all the markup responsibilities now belong to RepLogs.
    
    By the way, this is why smart components are often called "container" components: they are a container around a dumb, presentational component. People often even use that to name their components, like RepLogsContainer instead of RepLogApp.
    
    Anyways, I'm sitting here celebrating our genius, but this won't actually work yet: RepLogs needs a few pieces of data: withHeart and highlightedRowId. Both of these are available in RepLogApp. Oh, and we also need to pass a prop for the handleRowClick callback: that method also lives in RepLogApp.

---
+++ assets/js/RepLog/RepLogs.js
@@ -0,0 +1,59 @@
+import React, {Component} from "react";
+import RepLogList from "./RepLogList";
+
+export default function RepLogs(props) {
+	let heart = '';
+	if (withHeart){
+		heart = <span>❤️</span>;
+	}
+
+	return (
+		<div className="col-md-7">
+			<h2>Lift History! {heart}</h2>
+			<table className="table table-striped">
+				<thead>
+				<tr>
+					<th>What</th>
+					<th>How many times?</th>
+					<th>Weight</th>
+					<th>&nbsp;</th>
+				</tr>
+				</thead>
+				<RepLogList
+					highlightedRowId={ highlightedRowId }
+					onRowClick={ this.handleRowClick }
+				/>
+				<tfoot>
+				<tr>
+					<td>&nbsp;</td>
+					<th>Total</th>
+					<th>To do</th>
+					<td>
+
+					</td>
+				</tr>
+				</tfoot>
+			</table>
+
+			<form className="form-inline">
+				<div className="form-group">
+					<label className="sr-only required" htmlFor="item">What did you lift?</label>
+					<select id="rep_log_item" /* defaultValue="cat"*/ name="item" required="required">
+						<option value="">What did you lift?</option>
+						<option value="cat">Cat</option>
+						<option value="fat_cat">Big Fat Cat</option>
+						<option value="laptop">My Laptop</option>
+						<option value="coffee_cup">Coffee Cup</option>
+					</select>
+				</div>
+				{' '}
+				<div className="form-group">
+					<label className="sr-only required" htmlFor="reps">How many times?</label>
+					<input type="number" id="rep_log_reps" name="reps" required="required" placeholder="How many times?"/>
+				</div>
+				{' '}
+				<button type="submit" className="btn btn-primary">I Lifted it!</button>
+			</form>
+		</div>
+	);
+}
