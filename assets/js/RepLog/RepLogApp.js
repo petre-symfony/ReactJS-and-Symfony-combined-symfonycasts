22d5c58ee33b0076a3c793071e57fea418379525
petrero <petrero@gmail.com>
2019-12-09 12:39:14 +0200

    28.4.API Auth & State via AJAX{Processing the fetch data via Promises}
    componentDidMount
    
    Awesome! Let's use this to set our initial state! First, set the initial repLogs state to an empty array. Next, copy the getRepLogs() call and remove it. Instead, create a new method called componentDidMount() and paste this there. In the callback, use this.setState() to set repLogs to data.
    
    Before we talk about this, let's try it. Refresh! Woh! We have real data! Yes, yes, yes! We're showing the same data as our original app!
    
    Back to the code! Until this moment, render() was the only "special", React-specific, method in our class. But there are a few other special methods called "lifecycle" methods. The componentDidMount() method is one of those: if this exists, React calls it right after our component is rendered to the DOM. And this is the best place to make any AJAX requests needed to populate your initial state.
    
    Actually, we could have left this code in the constructor(). Because we're in a browser, they're almost the same. But, componentDidMount() is generally the recommended place.

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -9,17 +9,9 @@ export default class RepLogApp extends Component {
 	constructor(props) {
 		super(props);
 
-		getRepLogs().then((data) => {
-			console.log(data);
-		})
-
 		this.state = {
 			highlightedRowId: null,
-			repLogs: [
-				{ id: uuid(), reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
-				{ id: uuid(), reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
-				{ id: uuid(), reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
-			],
+			repLogs: [],
 			numberOfHearts: 1
 		}
 
@@ -29,6 +21,15 @@ export default class RepLogApp extends Component {
 		this.handleDeleteRepLog = this.handleDeleteRepLog.bind(this);
 	}
 
+	componentDidMount() {
+		getRepLogs()
+			.then((data) => {
+				this.setState({
+					repLogs: data
+				})
+			});
+	}
+	
 	handleRowClick(repLogId) {
 		this.setState({highlightedRowId: repLogId});
 	}
