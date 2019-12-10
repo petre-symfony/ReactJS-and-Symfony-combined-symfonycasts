c98faa07cb16069a0411c7f7f21141079699e605
petrero <petrero@gmail.com>
2019-12-10 17:31:59 +0200

    42.3.CSRF Protection Part 1{Creating the Event Susbcriber: php bin/console make:subscriber}
    When maker finishes install, run:
    
    php bin/console make:subscriber
    Call it ApiCsrfValidationSubscriber. And, listen to the kernel.request event. Done! This made one change: it created a new class in src/EventSubscriber.

---
+++ src/EventSubscriber/ApiCsrfValidationSubscriber.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class ApiCsrfValidationSubscriber implements EventSubscriberInterface{
+  public function onKernelRequest(GetResponseEvent $event){
+    // ...
+  }
+
+  public static function getSubscribedEvents(){
+    return [
+      'kernel.request' => 'onKernelRequest',
+    ];
+  }
+}
